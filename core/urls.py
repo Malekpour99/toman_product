299277c8bbfbd08772bc7d99fa5cb68d90b6bd82
Malekpour99 <work.malekpour99@gmail.com>
2025-02-05 14:41:01 +0330

    init: project initial status for operation
    Configured REST framework and swagger panel along with other project requirements

---
+++ core/urls.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+]
+
+# Swagger/ReDoc API documentation endpoints for DEBUG mode only
+if settings.DEBUG:
+    urlpatterns += [
+        path(
+            "",
+            SpectacularSwaggerView.as_view(url_name="schema"),
+            name="swagger-ui",
+        ),
+        path(
+            "schema/",
+            SpectacularAPIView.as_view(api_version="v1"),
+            name="schema",
+        ),
+        path(
+            "redoc/",
+            SpectacularRedocView.as_view(url_name="schema"),
+            name="redoc",
+        ),
+    ]
