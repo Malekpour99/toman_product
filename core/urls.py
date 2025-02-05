8ac6300dd4e04762b3be372314866dfe49cf617f
Malekpour99 <work.malekpour99@gmail.com>
2025-02-05 18:01:09 +0330

    feat: core URLs - added products URLs
    Added products URLs in the project's core URLs

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from drf_spectacular.views import (
     SpectacularAPIView,
@@ -9,6 +9,7 @@ from drf_spectacular.views import (
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("products/", include("products.urls")),
 ]
 
 # Swagger/ReDoc API documentation endpoints for DEBUG mode only
