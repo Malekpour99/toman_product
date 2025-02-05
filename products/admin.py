313e8929a1de80e93b23274cb989cd9110c6b813
Malekpour99 <work.malekpour99@gmail.com>
2025-02-05 18:05:32 +0330

    feat: products admin - registered Product & ProductImage models
    Registered Product and ProductImage models in the admin panel

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product, ProductImage
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "price",
+        "description",
+    ]
+
+
+@admin.register(ProductImage)
+class ProductImageAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "product",
+    ]
