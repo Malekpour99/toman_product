7a67dbe59330b1f1b7aa089420049be93c50771b
Malekpour99 <work.malekpour99@gmail.com>
2025-02-05 18:09:18 +0330

    feat: products views - created ProductViewSet
    Created ProductViewSet for CRUD operations on Product model

--- products/views.py
+++ products/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.parsers import MultiPartParser, FormParser
 
-# Create your views here.
+from .models import Product
+from .serializers import ProductSerializer
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.filter(is_deleted=False)
+    serializer_class = ProductSerializer
+    parser_classes = (MultiPartParser, FormParser)
+
+    def destroy(self, request, *args, **kwargs):
+        product = self.get_object()
+        # Delete associated images from storage
+        for image in product.images.all():
+            image.image.delete(user=request.user)
+        product.delete(user=request.user)
+        return Response(status=status.HTTP_204_NO_CONTENT)
