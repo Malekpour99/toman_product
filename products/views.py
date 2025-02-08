b8453f832782463901210cca510b1f8b2c1488d5
Malekpour99 <work.malekpour99@gmail.com>
2025-02-08 10:30:15 +0330

    fix: ProductViewSet - product destroy method
    Fixed product destroy method for deleting products and their relevant images

--- products/views.py
+++ products/views.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.parsers import MultiPartParser, FormParser
@@ -11,10 +12,11 @@ class ProductViewSet(viewsets.ModelViewSet):
     serializer_class = ProductSerializer
     parser_classes = (MultiPartParser, FormParser)
 
+    @transaction.atomic
     def destroy(self, request, *args, **kwargs):
         product = self.get_object()
         # Delete associated images from storage
         for image in product.images.all():
-            image.image.delete(user=request.user)
-        product.delete(user=request.user)
+            image.delete(user=request.user if request.user.is_authenticated else None)
+        product.delete(user=request.user if request.user.is_authenticated else None)
         return Response(status=status.HTTP_204_NO_CONTENT)
