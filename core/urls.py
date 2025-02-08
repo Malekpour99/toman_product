f99225293cb5343d81398391df1ec0b3f1b680ed
Malekpour99 <work.malekpour99@gmail.com>
2025-02-08 10:28:48 +0330

    feat: core URLs - added media URL path
    Included media URL path in the project's core URL paths for uploading and serving media files in the development environment

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
+from django.urls import path, include
+from django.conf.urls.static import static
+
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularRedocView,
@@ -31,3 +33,7 @@ if settings.DEBUG:
             name="redoc",
         ),
     ]
+
+# Serve media files in DEBUG mode only
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
